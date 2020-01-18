195257c67c727c351dd06991549546b0bc43c864
usymmij <usymmij@gmail.com>
2020-01-18 14:08:34 +0800

    fix: set flowRate to 0 in stop()

---
+++ src/main/java/raidzero/robot/submodules/EjectsBalls.java
@@ -0,0 +1,51 @@
+package raidzero.robot.submodules;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import raidzero.robot.Constants;
+
+public class EjectsBalls extends Submodule {
+
+    private static CANSparkMax epididymis;
+    private static EjectsBalls instance = null;
+
+    private static double flowRate = 0.0;
+
+    public static EjectsBalls getInstance() {
+        if (instance == null) {
+            instance = new EjectsBalls();
+        }
+        return instance;
+    }
+
+    private EjectsBalls() {
+        /**
+         * Epididymis Init
+         */
+        epididymis = new CANSparkMax(Constants.skinFlap, MotorType.kBrushless);
+        epididymis.setInverted(Constants.skinFlapInvert);
+        epididymis.setIdleMode(Constants.skinFlapIdle);
+    }
+
+    @Override
+    public void update(double timestamp) {
+    }
+
+    @Override
+    public void run() {
+        epididymis.set(flowRate);
+    }
+
+    @Override
+    public void stop() {
+        flowRate = 0;
+        epididymis.set(0);
+    }
+
+    public void shoot(double speed, boolean freeze) {
+        if(!freeze){
+            flowRate = speed;
+        }
+    }
+}
