af8fb53fa1d1ddbb6f6218875134503e486a8253
usymmij <usymmij@gmail.com>
2020-01-19 10:47:44 +0800

    chore: fixed whitespace and method name

--- src/main/java/raidzero/robot/wrappers/InactiveDoubleSolenoid.java
+++ src/main/java/raidzero/robot/wrappers/InactiveDoubleSolenoid.java
@@ -2,22 +2,22 @@ package raidzero.robot.wrappers;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-public class InactiveDoubleSolenoid extends DoubleSolenoid{
+public class InactiveDoubleSolenoid extends DoubleSolenoid {
     protected boolean active = false;
 
     public InactiveDoubleSolenoid(int FwdSolenoid, int RvrseSolenoid) {
         super(FwdSolenoid, RvrseSolenoid);
     }
  
-    public void setState(boolean state) {
-      active = state;
+    public void setActive(boolean state) {
+        active = state;
     }
   
     @Override
     public void set(Value value) {
-      if(!active) {
-        return;
-      }
-      super.set(value);
+        if(!active) {
+            return;
+        }
+        super.set(value);
     }
 }
