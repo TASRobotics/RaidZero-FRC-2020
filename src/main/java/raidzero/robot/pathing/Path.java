d17142f2b0e20f8ef4eadbb99862bb29a69fb5b8
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2020-01-29 15:21:30 +0800

    feat: convenience method for PathWeaver JSON files

--- src/main/java/raidzero/robot/pathing/Path.java
+++ src/main/java/raidzero/robot/pathing/Path.java
@@ -1,7 +1,11 @@
 package raidzero.robot.pathing;
 
-import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import java.io.IOException;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import raidzero.robot.Constants.DriveConstants;
 import raidzero.robot.submodules.Drive;
 
@@ -40,6 +44,24 @@ public abstract class Path {
      * @return path trajectory
      */
     protected abstract Trajectory generateTrajectory();
+
+    /**
+     * Returns a trajectory loaded from a JSON file.
+     * Note: PathWeaver files usually look like XXX.wpilib.json
+     * 
+     * @param filename name of the JSON path file
+     * @return loaded trajectory
+     */
+    protected Trajectory loadFromJson(String filename) {
+        try {
+            return TrajectoryUtil.fromPathweaverJson(
+                Filesystem.getDeployDirectory().toPath().resolve("paths/" + filename)
+            );
+        } catch (IOException exception) {
+            DriverStation.reportError("Unable to open trajectory: " + filename, exception.getStackTrace());
+        }
+        return null;
+    }
 
     /**
      * Returns the stored trajectory in this path.
