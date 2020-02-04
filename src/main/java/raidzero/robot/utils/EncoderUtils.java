73a9267f8ebcd04bcf2084ea6cab6483b8252b06
Louis Asanaka <25756888+LouisAsanaka@users.noreply.github.com>
2020-02-04 11:07:11 +0800

    fix: minor constant errors

--- src/main/java/raidzero/robot/utils/EncoderUtils.java
+++ src/main/java/raidzero/robot/utils/EncoderUtils.java
@@ -1,21 +1,21 @@
 package raidzero.robot.utils;
 
-import raidzero.robot.Constants;
+import raidzero.robot.Constants.DriveConstants;
 import raidzero.robot.submodules.Drive.GearShift;
 
 public class EncoderUtils {
 
     public static double ticksToInches(double ticks, GearShift gear) {
         if (gear == GearShift.HIGH) {
-            return ticks / Constants.SENSOR_UNITS_PER_INCH_HIGH_GEAR;
+            return ticks / DriveConstants.SENSOR_UNITS_PER_INCH_HIGH_GEAR;
         }
-        return ticks / Constants.SENSOR_UNITS_PER_INCH_LOW_GEAR;
+        return ticks / DriveConstants.SENSOR_UNITS_PER_INCH_LOW_GEAR;
     }
 
     public static double inchesToTicks(double inches, GearShift gear) {
         if (gear == GearShift.HIGH) {
-            return inches * Constants.SENSOR_UNITS_PER_INCH_HIGH_GEAR;
+            return inches * DriveConstants.SENSOR_UNITS_PER_INCH_HIGH_GEAR;
         }
-        return inches * Constants.SENSOR_UNITS_PER_INCH_LOW_GEAR;
+        return inches * DriveConstants.SENSOR_UNITS_PER_INCH_LOW_GEAR;
     }
 }
