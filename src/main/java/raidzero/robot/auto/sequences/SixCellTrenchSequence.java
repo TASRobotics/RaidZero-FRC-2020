e561f9a08467ce528d85403556252051206d347a
Raid Zero Driver Station <61722350+RaidZeroDS@users.noreply.github.com>
2020-03-12 09:24:21 +0800

    fix: consistent 6-cell & fixed turret action bug

--- src/main/java/raidzero/robot/auto/sequences/SixCellTrenchSequence.java
+++ src/main/java/raidzero/robot/auto/sequences/SixCellTrenchSequence.java
@@ -26,14 +26,14 @@ public class SixCellTrenchSequence extends AutoSequence {
     private static final Point[] TRENCH_BACKWARD_WAYPOINTS = {
         new Point(330, -24, 180),
         new Point(200, -24, 180),
-        new Point(120, -30, 180)//delete this
+        new Point(180, -24, 180)//delete this
         /*add these after testing, should help with accuracy and time
         new Point(332, -24, 180),
         new Point(170, -100, 180)
         */
     };
     private static final Path TRENCH_BACKWARD_PATH = new Path(TRENCH_BACKWARD_WAYPOINTS, true,
-        11.0, DriveConstants.DEFAULT_TARGET_ACCELERATION);
+        14, DriveConstants.DEFAULT_TARGET_ACCELERATION);
 
     private static final Drive drive = Drive.getInstance();
     private static final Intake intake = Intake.getInstance();
@@ -47,19 +47,21 @@ public class SixCellTrenchSequence extends AutoSequence {
     public void sequence() {
         addAction(new SeriesAction(
             Arrays.asList(
-                new LambdaAction(() -> drive.setBrakeMode(true)),
+                //new LambdaAction(() -> drive.setBrakeMode(true)),
                 new ParallelAction(
                     Arrays.asList(
+                        new LambdaAction(() -> shooter.shoot(2.0, false)),
+                        new WaitAction(2.0),
                         new SetShooterVelocity(1.0),
                         new LambdaAction(() -> intake.setPosition(Position.DOWN)),
                         new SeriesAction(Arrays.asList(
                             new TurnTurretToAngle(100),
                             new TurnToGoal()
                         )),
-                        new SetHoodPosition(6100)
+                        new SetHoodPosition(6180)
                     )
                 ),
-                new FeedBalls(2.0),
+                new FeedBalls(1.5),
                 //new LambdaAction(() -> shooter.stop()),
                 new LambdaAction(() -> intake.intakeBalls(1.0)),
                 new DrivePath(TRENCH_FORWARD_PATH)
@@ -69,13 +71,14 @@ public class SixCellTrenchSequence extends AutoSequence {
             Arrays.asList(
                 new ParallelAction(Arrays.asList(
                     new DrivePath(TRENCH_BACKWARD_PATH),
-                    new TurnToGoal(),
-                    new FeedBalls(0.6, true)
-                    //new SetShooterVelocity(1.0)                   
+                    new SetHoodPosition(6400),
+                    new TurnToGoal()
+                    //new FeedBalls(0.6, true)
+                    //new SetShooterVelocity(1.0)                 
                 )),
-                new LambdaAction(() -> drive.setBrakeMode(true)),
+                //new LambdaAction(() -> drive.setBrakeMode(true)),
                 new FeedBalls(5.0),
-                new LambdaAction(() -> drive.setBrakeMode(false)),
+                //new LambdaAction(() -> drive.setBrakeMode(false)),
                 new LambdaAction(() -> shooter.stop()),
                 new LambdaAction(() -> intake.stop())
             )
